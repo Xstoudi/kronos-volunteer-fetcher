6764c69c8ae4fe3b5d8e027d21ddfb7798c9fc8b
Xavier Stouder <xavier@stouder.io>
2019-07-02 17:51:56 +0200

    Updates

---
+++ precache-manifest.b78dbd6ff2b29f53bc782ae0aaf4f103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a898dc7a3f1df35fabf668f39b4485",
+    "url": "/kronos-volunteer-fetcher/index.html"
+  },
+  {
+    "revision": "39901383d90cc11a0a24",
+    "url": "/kronos-volunteer-fetcher/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/kronos-volunteer-fetcher/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "39901383d90cc11a0a24",
+    "url": "/kronos-volunteer-fetcher/static/js/main.efaedd18.chunk.js"
+  },
+  {
+    "revision": "a211d91257836207f8d1",
+    "url": "/kronos-volunteer-fetcher/static/js/runtime~main.5e5f3718.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kronos-volunteer-fetcher/static/media/logo.5d5d9eef.svg"
+  }
+]);
